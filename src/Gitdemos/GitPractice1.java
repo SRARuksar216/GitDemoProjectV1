0bddc47775b219e7026c8ce3db3f780c4cfac84f
RuksarKhan <RuksarKhan@10.9.91.204>
2024-08-28 12:23:57 +0530

    First Commit

---
+++ src/Gitdemos/GitPractice1.java
@@ -0,0 +1,11 @@
+package Gitdemos;
+
+public class GitPractice1 {
+
+	public static void main(String[] args) {
+		// // Print a greeting message to the console
+        System.out.println("Hello\nRuksar Khan from BC India!");
+
+	}
+
+}
