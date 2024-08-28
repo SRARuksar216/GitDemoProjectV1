7848dce40d31eb23bfbea4933b1fdb50a0dc6ef1
RuksarKhan <RuksarKhan@10.9.91.204>
2024-08-28 14:36:11 +0530

    Simple program for addition

---
+++ src/Gitdemos/GitP2.java
@@ -0,0 +1,20 @@
+package Gitdemos;
+
+public class GitP2 {
+
+	public static void main(String[] args) {
+		//public class SimpleAddition {
+	  
+	        // Define the numbers to be added
+	        int num1 = 5;
+	        int num2 = 10;
+	        
+	        // Calculate the sum
+	        int sum = num1 + num2;
+	        
+	        // Display the result
+	        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+	    }
+	}
+
+
